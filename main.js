9f752e379c8edf0e79067f516f948a170fc425c6
freeanke <freeanke@gmail.com>
2017-11-19 19:54:04 +0100

    Improved nav. Added responsiveness

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){   
+
+  $(document).scroll(function() { 
+    var scroll_start = $(this).scrollTop();
+    var windowsize = $(window).width();	  
+    if((scroll_start > 40) || ((scroll_start <= 40)&&(windowsize <= 768))){
+      $('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
+    } else if ((scroll_start <= 40)&&(windowsize > 768)){
+      $('#colorChange').css('background-color', 'transparent');
+    }
+  });
+   
+  $(window).resize(function() {
+    var windowsize = $(window).width();
+    var scroll_start = $(this).scrollTop();
+    if(windowsize <= 768) {   
+      $('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
+    } else if ((windowsize > 768)&&(scroll_start <= 40)){
+      $('#colorChange').css('background-color', 'transparent');
+    }
+  });
+  
+});
+
