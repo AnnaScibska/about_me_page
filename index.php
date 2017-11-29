8621b49da845a83c30352eed1788d5f0e4811dd2
freeanke <freeanke@gmail.com>
2017-11-30 00:42:49 +0100

    Section added: Badges

--- index.php
+++ index.php
@@ -1,133 +1,247 @@
 <!DOCTYPE html>
 <html lang="en">
-
-  <html>
   
-    <head>
+	<head>
 	
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	  
-	  <title>Anula | About me</title>
+		<title>Anula | About me</title>
+		
+		<link rel="icon" href="img/favicon.png" type="image/x-icon">
 	  
-	  <link href="vendor/bootstrap/bootstrap.css" rel="stylesheet">
-      <link href="main.css" rel="stylesheet">
+		<link href="vendor/bootstrap/bootstrap.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"  rel="stylesheet">
+		<link href="main.css" rel="stylesheet">
 	  
-	  <link href="https://fonts.googleapis.com/css?family=Rock+Salt" rel="stylesheet">	
-	  <link href="https://fonts.googleapis.com/css?family=Kavivanar&amp;subset=latin-ext" rel="stylesheet">
-	  <link href="https://fonts.googleapis.com/css?family=Carrois+Gothic+SC" rel="stylesheet">
-	  <link rel="icon" href="img/favicon.png" type="image/x-icon"></link>	  
+		<link href="https://fonts.googleapis.com/css?family=Rock+Salt" rel="stylesheet">	
+		<link href="https://fonts.googleapis.com/css?family=Kavivanar&amp;subset=latin-ext" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Carrois+Gothic+SC" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 	  
 	</head>
 	
 	<body id="page-top">
 	
-	  <div class="container-fluid">
-		<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="colorChange">
-		  <a href="#page-top" class="navbar-header">Anula</a>
-		  <button class="navbar-toggler navbar-toggler-color-change" type="button" data-toggle="collapse" data-target="#myNavbar">
-		  Menu<span class="navbar-toggler-icon icon-color-change "></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="myNavbar">
-			<ul class="navbar-nav ml-auto">
-			  <li class="nav-item">
-				<a class="nav-link nav-link-color-change" href="#about">about </a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link nav-link-color-change" href="#badges">learning badges</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link nav-link-color-change" href="#code">code sample</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link nav-link-color-change" href="#contact">contact</a>
-			  </li>
-			</ul>
-		  </div>
-		</nav>
-	  </div>
-	  
-	  <header>
-		<h1>Anula</h1>
-	  </header>
-	  
-	  <section id="about">
-		<div class="pwrap">
-		  <p class="temporary">/ about / <br/> work still in progress</p>
+		<div class="container-fluid">
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="colorChange">
+				<a href="#page-top" class="navbar-header">Anula</a>
+			<button class="navbar-toggler navbar-toggler-color-change" type="button" data-toggle="collapse" data-target="#myNavbar">
+			Menu<span class="navbar-toggler-icon icon-color-change "></span>
+			</button>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link nav-link-color-change" href="#about">about </a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link nav-link-color-change" href="#badges">learning badges</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link nav-link-color-change" href="#code">code sample</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link nav-link-color-change" href="#contact">contact</a>
+					</li>
+				</ul>
+			</div>
+			</nav>
 		</div>
-	  </section>
+		
+		<header>
+			<h1>Anula</h1>
+		</header>
 	  
-	  <section id="badges">
-	    <div class="pwrap">
-		  <p class="temporary">/ badges / <br/> work still in progress</p>
-		</div>
-	  </section>
+		<section id="about">
+			<h2>/ about / <br/> work still in progress</h2>
+		</section>
 	  
-	  <section id="code">
-	    <div class="pwrap">
-		  <p class="temporary">/ code / <br/> work still in progress</p>
-		</div>
-	  </section>
-	  
-	  <section id="contact">
-	    <div class="pwrap">
-		  <p class="temporary">Contact me!</p>
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."#contact");?>" id="contactForm"> 
-			  <div class="container">
+		<section id="badges">
+			<div class="container">
+				<h2>learning badges and certificates</h2>
+				<h3>languages, frameworks & tools I got to know. my level of knowledge of the basis is represented by the number of stars.</h3>
+				<p>clik to see badges and certificates.</p>
 				<div class="row">
-				  <div class="form-group d-flex flex-column col-md-4">
-					<label for="contactname">Name :</label>
-					<input type="text" name="contactname" id="contactname" placeholder="Google" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Google'" required>
-					<label for="contactemail">Email :</label>
-					<input type="email" name="contactemail" id="contactemail" placeholder="google@google.com" onfocus="this.placeholder=''" onblur="this.placeholder='google@google.com'" required>
-				  </div>
-				  <div class="form-group d-flex flex-column col-md-8">
-					<label for="message">Message :</label>
-					<textarea name="message" rows="6" id="message" placeholder="We want to hire you as a CEO. Or as a Very Junior Web Developper. Contact us ASAP!" onfocus="this.placeholder=''" onblur="this.placeholder='We want to hire you as a CEO. Or as a Very Junior Web Developper. Contact us ASAP!'" ></textarea>
-				  </div>
+					<div class="d-flex flex-column align-items-center col-md-6">
+						<a href="img/badges/htmlOC.png" data-toggle="lightbox" data-gallery="HTML">
+							<div class="language">
+								<p>HTML</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>	
+						<a href="img/badges/htmlCC.png" data-toggle="lightbox" data-gallery="HTML"></a>
+						<a href="img/badges/htmlSL.jpg" data-toggle="lightbox" data-gallery="HTML"></a>		
+						<a href="img/badges/cssOC.png" data-toggle="lightbox" data-gallery="CSS">
+							<div class="language">
+								<p>CSS</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/cssCC.png" data-toggle="lightbox" data-gallery="CSS"></a>
+						<a href="img/badges/cssSL.jpg" data-toggle="lightbox" data-gallery="CSS"></a>
+						<a href="img/badges/bootstrapCC.png" data-toggle="lightbox" data-gallery="bootstrap">
+							<div class="language">
+								<p>Bootstrap</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/javascriptOC.png" data-toggle="lightbox" data-gallery="javascript">
+							<div class="language">
+								<p>JavaScript</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/javascriptCC.png" data-toggle="lightbox" data-gallery="javascript"></a>
+						<a href="img/badges/javascriptSL.jpg" data-toggle="lightbox" data-gallery="javascript"></a>
+					</div>
+					<div class="d-flex flex-column align-items-center col-md-6">
+						<a href="img/badges/jQueryCC.png" data-toggle="lightbox" data-gallery="jQuery">
+							<div class="language">
+								<p>jQuery</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/jQuerySL.jpg" data-toggle="lightbox" data-gallery="jQuery"></a>
+						<a href="img/badges/phpCC.png" data-toggle="lightbox" data-gallery="php">
+							<div class="language">
+								<p>PHP</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/phpSL.jpg" data-toggle="lightbox" data-gallery="php"></a>
+						<a href="img/badges/gitCC.png" data-toggle="lightbox" data-gallery="git">
+							<div class="language">
+								<p>git</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+						<a href="img/badges/sqlCC.png" data-toggle="lightbox" data-gallery="sql">
+							<div class="language">
+								<p>SQL</p>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+						</a>
+					</div>
 				</div>
-				<button type="submit" name="submit" form="contactForm" value="Submit" class="submitBtn">SEND</button> 
-			  </div>
-			</form>
-		  
-		  	<?php
-			  $servername = ********;
-			  $username = ********;
-			  $password = ********;
-			  $dbname = ********;
-				
-			  $conn = new mysqli($servername, $username, $password, $dbname);
-			  if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			  }
-				
-			  if (isset($_POST['submit'])) {
-				$contactname = $_POST['contactname'];
-				$contactemail = $_POST['contactemail'];
-				$message = $_POST['message'];
-					
-				$sql = "INSERT INTO contactRecords (contactnameSQL, contactemailSQL, messageSQL) VALUES ('$contactname', '$contactemail', '$message')";
-
-				if ($conn->query($sql) === TRUE) {
-				  echo "<p class='thxMessage'>Thank you for sending a message :)</p>";
-				} else {
-				  echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-			  }
-			?>
-			
-		</div>
-	  </section>
+				<h3>See my online profiles:</h3>
+				<div class="container onlineProfiles">
+					<div class="row d-flex justify-content-center">
+						<a href="https://www.codecademy.com/anna.scibska">
+							<img src="img/logo/CodeCademy.png" alt="Link to CodeCademy profile." onmouseover="this.src='img/logo/CodeCademyReverse.png';" onmouseout="this.src='img/logo/CodeCademy.png';" class="col-md onlineProfile"/>
+						</a>
+						<a href="https://openclassrooms.com/membres/annascibska">
+							<img src="img/logo/OpenClassroom.png" alt="Link to Open Classroom profile." onmouseover="this.src='img/logo/OpenClassroomReverse.png';" onmouseout="this.src='img/logo/OpenClassroom.png';" class="col-md onlineProfile"/>
+						</a>
+						<a href="https://github.com/freeanke">
+							<img src="img/logo/GitHub.png" alt="Link to GitHub profile." onmouseover="this.src='img/logo/GitHubReverse.png';" onmouseout="this.src='img/logo/GitHub.png';" class="col-md onlineProfile"/>
+						</a>
+					</div>
+					<div class="row d-flex justify-content-center">
+						<a href="https://www.sololearn.com/Profile/6550246">
+							<img src="img/logo/SoloLearn.png" alt="Link to SoloLearn profile." onmouseover="this.src='img/logo/SoloLearnReverse.png';" onmouseout="this.src='img/logo/SoloLearn.png';" class="col-md onlineProfile"/>
+						</a>
+						<a href="https://codecombat.com/user/anulla">
+							<img src="img/logo/CodeCombat.png" alt="Link to CodeCombat profile." onmouseover="this.src='img/logo/CodeCombatReverse.png';" onmouseout="this.src='img/logo/CodeCombat.png';" class="col-md onlineProfile"/>
+						</a>
+					</div>
+				</div>	
+			</div>
+		</section>
 	  
-	  <footer>
-	    <p>Copyright &copy; Anula 2017</p>
-	  </footer>
+		<section id="code">
+			<h2>/ code / <br/> work still in progress</h2>
+		</section>
+	  
+		<section id="contact">
+			<div class="container">
+				<h2>Contact me!</h2>
+				<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."#contact");?>" id="contactForm"> 
+					<div class="row">
+						<div class="form-group d-flex flex-column col-md-4">
+							<label for="contactname">Name :</label>
+							<input type="text" name="contactname" id="contactname" placeholder="Google" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Google'" required>
+							<label for="contactemail">Email :</label>
+							<input type="email" name="contactemail" id="contactemail" placeholder="google@google.com" onfocus="this.placeholder=''" onblur="this.placeholder='google@google.com'" required>
+						</div>
+						<div class="form-group d-flex flex-column col-md-8">
+							<label for="message">Message :</label>
+							<textarea name="message" rows="6" id="message" placeholder="We want to hire you as a CEO. Or as a Very Junior Web Developper. Contact us ASAP!" onfocus="this.placeholder=''" onblur="this.placeholder='We want to hire you as a CEO. Or as a Very Junior Web Developper. Contact us ASAP!'" ></textarea>
+						</div>
+					</div>
+				</form>
+				<button type="submit" name="submit" form="contactForm" value="Submit" id="submitBtn">SEND <i class="fa fa-envelope-o"></i></button> 
+		  
+				<?php
+					$servername = "********";
+					$username = "********"; 
+					$password = "********"; 
+					$dbname = "********";
+				
+					$conn = new mysqli($servername, $username, $password, $dbname);
+					if ($conn->connect_error) {
+						die("Connection failed: " . $conn->connect_error);
+					}
+				
+					if (isset($_POST['submit'])) {
+						$contactname = $_POST['contactname'];
+						$contactemail = $_POST['contactemail'];
+						$message = $_POST['message'];
+					
+						$sql = "INSERT INTO contactRecords (contactnameSQL, contactemailSQL, messageSQL) VALUES ('$contactname', '$contactemail', '$message')";
 
-	  <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
-	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	  <script src="vendor/bootstrap/bootstrap.js"></script>
-	  <script src="main.js"></script>
+						if ($conn->query($sql) === TRUE) {
+							echo "<p class='thxMessage'>Thank you for sending a message :)</p>";
+						} else {
+							echo "Error: " . $sql . "<br>" . $conn->error;
+						}
+					}
+				?>
+			
+			</div>
+		</section>
+	  
+		<footer>
+			<p>Copyright &copy; Anula 2017</p>
+		</footer>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+		<script src="vendor/bootstrap/bootstrap.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js"></script>
+		<script src="main.js"></script>
 	
 	</body>
-	
-  </html>
+</html>
--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 $(document).ready(function(){   
 
-  $(document).scroll(function() { 
-    var scroll_start = $(this).scrollTop();
-    var windowsize = $(window).width();	  
-    if((scroll_start > 40) || ((scroll_start <= 40)&&(windowsize <= 768))){
-      $('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
-    } else if ((scroll_start <= 40)&&(windowsize > 768)){
-      $('#colorChange').css('background-color', 'transparent');
-    }
-  });
+	$(document).scroll(function() { 
+		var scroll_start = $(this).scrollTop();
+		var windowsize = $(window).width();	  
+		if((scroll_start > 40) || ((scroll_start <= 40)&&(windowsize <= 767))){
+			$('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
+			$('#colorChange').css('border-bottom', '1px solid #000');
+		} else if ((scroll_start <= 40)&&(windowsize > 767)){
+			$('#colorChange').css('background-color', 'transparent');
+			$('#colorChange').css('border', 'none');
+		}
+	});
    
-  $(window).resize(function() {
-    var windowsize = $(window).width();
-    var scroll_start = $(this).scrollTop();
-    if(windowsize <= 768) {   
-      $('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
-    } else if ((windowsize > 768)&&(scroll_start <= 40)){
-      $('#colorChange').css('background-color', 'transparent');
-    }
-  });
+	$(window).resize(function() {
+		var windowsize = $(window).width();
+		var scroll_start = $(this).scrollTop();
+		if(windowsize <= 767) {   
+			$('#colorChange').css('background-color', 'rgba(91, 91, 91, 0.9)');
+			$('#colorChange').css('border-bottom', '1px solid #000');
+		} else if ((windowsize > 767)&&(scroll_start <= 40)){
+			$('#colorChange').css('background-color', 'transparent');
+			$('#colorChange').css('border', 'none');
+		}
+	});
   
-  $('a').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+	$('a').click(function() {
+		$('.navbar-collapse').collapse('hide');
+	});
   
-  if ($("p.thxMessage").length) {
-	$("form").css("display", "none");
-  }
+	if ($('p.thxMessage').length) {
+		$('form').css('display', 'none');
+		$('#submitBtn').css('display', 'none');
+	}
+  
+	// lightbox gallery: http://ashleydw.github.io/lightbox/
+  
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+		event.preventDefault();
+		$(this).ekkoLightbox();
+	});
   
 });
 
